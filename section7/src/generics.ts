4cd1cf7418147f7f0b62169c154f87a4ac9bef8a
ritsuki-cs <game.ricky.x@gmail.com>
2022-07-28 19:38:25 +0900

    finish section7

---
+++ section7/src/generics.ts
@@ -0,0 +1,59 @@
+function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
+  value[key];
+  return value;
+}
+console.log(copy({ name: 'Quill', age: 38 }, 'name'));
+
+class LightDatabase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get() {
+    return this.data;
+  }
+}
+const stringLightDatabase = new LightDatabase();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add("Grape");
+stringLightDatabase.remove("Banana");
+console.log(stringLightDatabase.get());
+
+interface Todo {
+  title: string;
+  text: string;
+}
+type Todoable = Partial<Todo>
+type ReadTodo = Readonly<Todo>
+
+const fetchData: Promise<string> = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('hello');
+  }, 3000);
+})
+fetchData.then(data => {
+  data.toUpperCase();
+})
+const vegetables: Array<string> = ['Tomato', 'Broccoi', 'Asparagus'];
+
+interface ResponseData<T extends { message: string } = any> {
+  data: T;
+  status: number;
+}
+let GenericsTmp2: ResponseData;
+interface Vegetables {
+  readonly tomato: string;
+  pumpkin?: string;
+}
+let GenericsTmp3: keyof Vegetables;
+type MappedTypes = {
+  -readonly [P in keyof Vegetables]-?: string
+}
+type ConditionalTypes = 'tomato' extends string ? number : boolean;
+type ConditionalTypesInfer = { tomato: 'tomato' } extends { tomato: infer R } ? R : boolean;
+type DistributiveConditionalTypes<T> = T extends 'tomato' ? number : boolean;
+let GenericsTmp4: DistributiveConditionalTypes<'tomato' | 'pumpkin'>
